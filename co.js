7d70bd9b6f8466f9b4d8f05af8bf8f445aa80d7f
tzwel <tzwel@int.pl>
2023-06-05 15:27:53 +0200

    0.3 update

--- co.js
+++ co.js
@@ -1,19 +1,15 @@
 function co(args, autoSlashes) {
     let result = args.toString().length > 0 ? args : '.'
 
-    if (args.toString().length > 0) {
-        if (autoSlashes) {
-            result = args.join('/')
-        } else {
-            result = args.join('')
-        } 
-    }
+    if (args.toString().length > 0 && autoSlashes) {
+        result = args.join('/')
+    } else {
+        result = args.join('')
+    } 
         
-    result = result.replaceAll('\\', '/')
+    return result.replaceAll('\\', '/')
     .replace(/\/{2,}/g, '/')
     .replace(/\/+$/g, '')
-
-    return result
 }
 
 module.exports = co
